7bb270f746e4cc60ad54cef2f092df1b842000d9
Caleb <marshallc@ohio.edu>
2023-02-13 17:40:46 -0500

    Basic command line args.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,26 @@ mod bitmasks;
 mod mdpp16_scp;
 mod sort;
 mod write_data;
+use std::env;
 
 fn main() {
-    // and there is a package...
-    let contents = fs::read("/home/caleb/midas-tests/run00105.mid").unwrap();
+    // parse the command line args
+    let args: Vec<String> = env::args().collect();
+
+    let input_file: &str = &args[1];
+    let output_file: &str = if args.len() >= 3 {
+        &args[2]
+    } else {
+        panic!("Need output file name!")
+    };
+
+    //    let output_file = &output_file.replace(".mid", ".csv");
+
+    // see midasio package documentation for details
+    let contents = fs::read(input_file).unwrap();
     // lets try to decompress the file
     let file_view = FileView::try_from(&contents[..]).unwrap();
-    let sorter = sort::DataSort::new("test.csv".to_string(), 100);
+    let sorter = sort::DataSort::new(output_file.to_string(), 100);
     let mut data = sorter.sort_loop(&file_view);
-    data.write_data("test.csv");
+    data.write_data(output_file);
 }
