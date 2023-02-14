a5a75fe19278fe7c7529ff2406a5568dc3797465
Caleb <marshallc@ohio.edu>
2023-02-14 14:42:44 -0500

    Added basic clap support for command line arguments.

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,30 @@ mod bitmasks;
 mod mdpp16_scp;
 mod sort;
 mod write_data;
+use clap::Parser;
 use std::env;
+
+// I ripped this straight from the clap documentation
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    input_file: String,
+    output_file: String,
+    #[arg(long, default_value_t = 100000)]
+    chunk_size: usize,
+}
 
 fn main() {
     // parse the command line args
-    let args: Vec<String> = env::args().collect();
-
-    let input_file: &str = &args[1];
-    let output_file: &str = if args.len() >= 3 {
-        &args[2]
-    } else {
-        panic!("Need output file name!")
-    };
+    let args = Args::parse();
 
     //    let output_file = &output_file.replace(".mid", ".csv");
 
     // see midasio package documentation for details
-    let contents = fs::read(input_file).unwrap();
+    let contents = fs::read(args.input_file).unwrap();
     // lets try to decompress the file
     let file_view = FileView::try_from(&contents[..]).unwrap();
-    let sorter = sort::DataSort::new(output_file.to_string(), 100);
+    let sorter = sort::DataSort::new(args.output_file.to_string(), args.chunk_size);
     let mut data = sorter.sort_loop(&file_view);
-    data.write_data(output_file);
+    data.write_data(&args.output_file);
 }
