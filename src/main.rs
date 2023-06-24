4d185f3d6e22a0c559b524d88b6abb65d3c5356e
Caleb <marshallc@ohio.edu>
2023-06-24 16:29:14 -0400

    Parquet support and better data types.

--- src/main.rs
+++ src/main.rs
@@ -29,8 +29,12 @@ struct Args {
     chunk_size: usize,
     #[arg(long, short, default_value_t = false)]
     diagnostic: bool,
-    #[arg(long, short, default_value_t = true)]
+    #[arg(long, short, default_value_t = false)]
     csv: bool,
+    #[arg(long, short, default_value_t = true)]
+    parquet: bool,
+    #[arg(long, short, default_value_t = false)]
+    feather: bool,
 }
 
 fn main() {
@@ -118,33 +122,44 @@ fn main() {
             .status()
             .expect("Failed to delete file.");
     }
+
+    // create all of the output formats that the user wants
+
     // output parquet path buffer for the parquet_sink method.
-    let output_file_wo_csv = Path::new(&format!(
+    let output_file_parquet = Path::new(&format!(
+        "{}{}",
+        output_file.split('.').next().unwrap(),
+        ".parquet"
+    ))
+    .to_path_buf();
+
+    let output_file_feather = Path::new(&format!(
         "{}{}",
         output_file.split('.').next().unwrap(),
         ".feather"
     ))
     .to_path_buf();
-    //let mut p_file = File::create(output_file_wo_csv).expect("could not create file");
 
     // spinner while we convert
     let pb = ProgressBar::new_spinner();
     pb.enable_steady_tick(Duration::from_millis(200));
     pb.tick();
-    pb.set_message("Converting to feather...");
+    pb.set_message("Converting to final formats...");
+
     // This should stream the csv off the disk and periodically dump to the parquet file.
-    // let p_w_args = ParquetWriteOptions {
-    //     compression: ParquetCompression::Lz4Raw,
-    //     statistics: false,
-    //     row_group_size: Some(60 * 1024 * 1024),
-    //     data_pagesize_limit: None,
-    //     maintain_order: true,
-    // };
+    let p_w_args = ParquetWriteOptions {
+        compression: ParquetCompression::Lz4Raw,
+        statistics: false,
+        row_group_size: Some(60 * 1024 * 1024),
+        data_pagesize_limit: None,
+        maintain_order: true,
+    };
 
     let ipc_args = IpcWriterOptions {
         compression: None,
         maintain_order: true,
     };
+
     let mut sch = Schema::new();
 
     let columns = vec![
@@ -160,15 +175,15 @@ fn main() {
     ];
 
     let col_types = vec![
-        DataType::UInt8,
-        DataType::UInt8,
         DataType::UInt32,
         DataType::UInt32,
         DataType::UInt32,
         DataType::UInt32,
         DataType::UInt32,
+        DataType::UInt32,
+        DataType::UInt64,
         DataType::Boolean,
-        DataType::UInt32,
+        DataType::UInt64,
     ];
 
     for (&col, dt) in columns.iter().zip(col_types.iter()) {
@@ -176,18 +191,32 @@ fn main() {
         s.push_str(col);
         sch.with_column(s, dt.to_owned());
     }
-    println!("Schema set.");
-    // It works for ipc, weird...
-    LazyCsvReader::new(&output_file)
-        .has_header(true)
-        //.with_dtype_overwrite(Some(&sch)) still broken
-        .finish()
-        .unwrap()
-        .with_type_coercion(true)
-        .sink_ipc(output_file_wo_csv, ipc_args)
-        .expect("Error writing feather file.");
 
-    pb.finish_with_message("Conversion done!");
+    if args.parquet {
+        // Parquet dump
+        LazyCsvReader::new(&output_file)
+            .has_header(true)
+            .with_dtype_overwrite(Some(&sch))
+            .finish()
+            .unwrap()
+            .with_type_coercion(true)
+            .sink_parquet(output_file_parquet, p_w_args)
+            .expect("Error writing parquet file.");
+
+        pb.println("Parquet conversion done!");
+    }
+    if args.feather {
+        // feather dump
+        LazyCsvReader::new(&output_file)
+            .has_header(true)
+            .with_dtype_overwrite(Some(&sch))
+            .finish()
+            .unwrap()
+            .with_type_coercion(true)
+            .sink_ipc(output_file_feather, ipc_args)
+            .expect("Error writing feather file.");
+        pb.finish_with_message("Feather conversion done!");
+    }
     // delete the csv if you want to cover your tracks
     if !args.csv {
         Command::new("rm")
